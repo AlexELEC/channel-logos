e401db23937c32477a56544abd654472d00c3b5c
Alexandr <alex@alexelec.in.ua>
2017-01-23 12:14:40 +0200

    Init.

---
+++ create.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+################################################################################
+#      This file is part of Alex@ELEC - http://www.alexelec.in.ua
+#      Copyright (C) 2011-2017 Alexandr Zuyev (alex@alexelec.in.ua)
+################################################################################
+
+echo "packing sources..."
+  tar jcvf logos.tar.bz2 ./backgrounds ./logos
